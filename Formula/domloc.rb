1a6f5b175937ec298c4cfdf2910f3e0c31ce77ce
wemit <7489848+wemit@users.noreply.github.com>
2026-05-15 00:59:10 +0300

    Update formula SHA256 for v0.1.0

--- Formula/domloc.rb
+++ Formula/domloc.rb
@@ -6,20 +6,20 @@ class Domloc < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/wemit/domloc/releases/download/v#{version}/domloc-darwin-arm64.tar.gz"
-      sha256 "REPLACE_DARWIN_ARM64_SHA256"
+      sha256 "4d1695a5ae6c94a648042085c8264d5146915028621cf9db7c174709d856f780"
     else
       url "https://github.com/wemit/domloc/releases/download/v#{version}/domloc-darwin-amd64.tar.gz"
-      sha256 "REPLACE_DARWIN_AMD64_SHA256"
+      sha256 "2f5c0b001cf5de0ff7372eed5860ee87383c8f61b0593a38371ea4525915389e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/wemit/domloc/releases/download/v#{version}/domloc-linux-arm64.tar.gz"
-      sha256 "REPLACE_LINUX_ARM64_SHA256"
+      sha256 "1a86c0a6f64767247436fcac108b9b078a726999f5fcd0ecfd05ee0fb21b58ad"
     else
       url "https://github.com/wemit/domloc/releases/download/v#{version}/domloc-linux-amd64.tar.gz"
-      sha256 "REPLACE_LINUX_AMD64_SHA256"
+      sha256 "d039dd16ad2f97e728ceef5fc239e1ba1bbb485b5a09b4067f6fc6786f66a983"
     end
   end
 
